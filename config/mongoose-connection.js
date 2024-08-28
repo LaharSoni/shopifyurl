ba4944de9c5dabdbf432cc97626673fe47866446
LaharSoni <lahar.soni410@gmail.com>
2024-08-28 12:39:10 +0530

    six

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017').then(function(){
+mongoose.connect(process.env.MONGO_DB_URI).then(function(){
     console.log('Connected to MongoDB');
 })
 
