570b1588a7150044b91ec4b47b8b0b0a409a8ccd
LaharSoni <lahar.soni410@gmail.com>
2024-08-28 12:19:54 +0530

    third

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const shopifyRouter = require('./routes/shopify-router')
 const orderRouter = require('./routes/order-router')
 const productRouter = require('./routes/product-router')
 
-const db = require('./config/mongoose-connection')
+// const db = require('./config/mongoose-connection')
 // require('dotenv').config();
 
 
@@ -22,8 +22,8 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use('/', shopifyRouter);
-app.use('/order', orderRouter);
-app.use('/product', productRouter);
+// app.use('/order', orderRouter);
+// app.use('/product', productRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
